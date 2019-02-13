4644220834389598df43780d8ef50de61345a6b5
Peter Jin <peter@guppybot.org>
2019-02-13 10:22:42 -0800

    Add the guppybot target (currently just a no-op executable).

--- tools/guppyctl-bin/main.rs
+++ tools/guppyctl-bin/main.rs
@@ -1,5 +1,7 @@
 extern crate guppyctl;
 
+static GUPPYBOT_BIN: &'static [u8] = include_bytes!("../guppybot");
+
 fn main() {
-  guppyctl::cli::dispatch(&[]);
+  guppyctl::cli::dispatch(GUPPYBOT_BIN);
 }
